23f27a0cdc0c12d60fd56182e88fce639e690d1b
eduardocastaneda <eduardo.castaneda25@gmail.com>
2022-07-07 23:10:59 +0200

    feat: allow nullable report in report query

--- src/typeDefs/Query.js
+++ src/typeDefs/Query.js
@@ -3,6 +3,6 @@ const { gql } = require('apollo-server');
 module.exports = gql`
   type Query {
     users: [User]
-    report(year: Int!, month: Int!): Report!
+    report(year: Int!, month: Int!): Report
   }
 `;
